19eec67c37afd102404d81034ae8687b1455471c
george-iot <georgeshenoda@protonmail.com>
2026-03-24 01:01:52 +0200

    fixing admin

--- admin/app/(main)/page.tsx
+++ admin/app/(main)/page.tsx
@@ -13,7 +13,7 @@ export default function Home() {
                   اضف ملابس
                 </Link>
               </div>
-                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+                <div>
                     <CardItem />
                 </div>
             </div>
