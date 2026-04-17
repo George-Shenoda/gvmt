8bf895aaaea19f435172993130f0040b1f8a4328
George Shenoda <georgeshenoda@protonmail.com>
2026-04-17 11:04:15 +0200

    Update message in CardComponent when no clothes available

--- admin/components/web/CardComponent.tsx
+++ admin/components/web/CardComponent.tsx
@@ -75,7 +75,7 @@ export default function CardItem() {
     if (!clothes || clothes.length === 0 || !isSuccess) {
         return (
             <div className="container mx-auto w-screen h-[calc(100vh-8rem)] flex items-center justify-center">
-                <h2 className="text-2xl font-bold">مفيش لبس他现在</h2>
+                <h2 className="text-2xl font-bold">مفيش لبس</h2>
             </div>
         );
     }
